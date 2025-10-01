7cbf618f8fecbab2f1274c92385655cd611c8fde
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-30 22:29:17 -0300

    A

---
+++ godot/scripts/ui/hud.gd
@@ -0,0 +1,5 @@
+extends Control
+
+@onready var health_bar: ProgressBar = $MarginContainer/VBoxContainer/StatsBars/HealthBar
+@onready var mana_bar: ProgressBar = $MarginContainer/VBoxContainer/StatsBars/ManaBar
+@onready var lucy_state_label: Label = $MarginContainer/VBoxContainer/StateLabels/LucyStateLabel
